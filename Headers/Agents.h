cc3cf250ef58e455a20f878493630f0840c298de
Michael Williams <mjwillow117@hotmail.com>
2012-09-27 15:12:19 +0100

    Moved BM and rand functions for issue #8
    Part fixes issue #8, see issue comments for details

--- Headers/Agents.h
+++ Headers/Agents.h
@@ -5,6 +5,7 @@
 #include<string>
 #include<vector>
 #include<cmath>
+#include<ctime>
 
 namespace org{
 
@@ -16,6 +17,7 @@ namespace org{
 		virtual ~gene(){}
 		virtual void mutate()=0; // how does this gene mutate?
 		virtual bool selection()=0; //may need to be numerical later- how is this gene selected
+		
 	
 	};
 	
@@ -35,12 +37,38 @@ namespace org{
 		virtual ~height(){}
 		
 		virtual void mutate(){
-		
-			//mutate this gene! (currently increase height by 1)
-			
-			this->value++;
-			
+			double mean = value;
+			double sd=rate;
+			double x1, x2, w, z1; //x1 and x2 are the random numbers defined in Boxmuller function
+			static double z2; //Z2 is the second distributed value
+			static int use_last = 0; //The box-muller function generates two values. It is more useful to save this value for the second call
+			srand((unsigned)time(0)); //Seeds random numbers based on time of execution;
+			if (use_last)		        
+			{
+				z1 = z2;
+				use_last = 0;
+			}
+			else
+			{
+				do {
+					double RandomNumber = (double(rand()) / double(RAND_MAX)) * (1 - 0) + 0; //Gives a random number between 0 and 1
+					x1 = 2.0 * RandomNumber - 1.0;
+					RandomNumber = (double(rand()) / double(RAND_MAX)) * (1 - 0) + 0; //Get a new random number between 0 and 1
+					x2 = 2.0 * RandomNumber - 1.0; 
+					w = x1 * x1 + x2 * x2;
+				} while ( w >= 1.0 );
+
+				w = sqrt( (-2.0 * log( w ) ) / w );
+				z1 = x1 * w;
+				z2 = x2 * w;
+				use_last = 1;
+			}
+
+			value = ( mean + z1 * sd ); //To get any mean/sd we scale wrt sd and transform wrt mean (source: http://en.literateprograms.org/Box-Muller_transform_%28C%29)
 		}
+		
+			
+		
 		
 		virtual bool selection(){
 			//taller is better kk.
