4667f83df0f1743d7cc929265343dabb3b3c960b
TaranMoltu <taranmoltu@gmail.com>
2013-01-31 02:11:49 +0000

    Added the basic sexual reproduction- now sexy times are possible!

---
+++ HeaderCode/Reproduction/sex.cpp
@@ -0,0 +1,46 @@
+/*
+ * sex.cpp
+ *
+ *  Created on: 30 Jan 2013
+ *      Author: joshua
+ */
+
+#include "../../Headers/entities.h"
+
+using namespace org;
+
+/*============================================================================
+ * Gene Mating
+ *========================================================================== */
+void height::mate(gene* spouceGene){
+	value=(spouceGene->getValue()+this->getValue())/2.0;
+}
+void genome::mate(genome* spouceGenome){
+	std::vector<gene*>::const_iterator current,begin,end;
+	begin = genes.begin();
+	end = genes.end();
+	unsigned i(0);
+
+	for (current=begin; current<end;++current){//mate all genes
+		(*current)->mate((*spouceGenome)(i));
+		i++;
+	}
+
+}
+
+
+/*============================================================================
+ * Sexual reproduction
+ *========================================================================== */
+
+entity* entity::sex(entity* spouse){
+	if (false){
+		return NULL; //return null pointer if match is not appropriate
+	}
+	else {
+		entity* temp = this->clone();
+		temp->mate(spouse);
+		temp->mutate();
+		return temp;
+	}
+}
