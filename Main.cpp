c888a59892ec51f54c33befa26669123dee8ce11
TaranMoltu <taranmoltu@gmail.com>
2013-01-31 15:40:26 +0000

    quick fixes, just ease of use things really.

--- Main.cpp
+++ Main.cpp
@@ -11,21 +11,22 @@
 
 int main(){
 	clock_t begin=clock();
-	double carryingCapacity(20000);
+	double carryingCapacity(500);
 	double standDeviation(0.01); //variation in height
-	double reach(4.0);
-	unsigned int loops(10000); //Loops is the number of generations - can change safely
+	double reach(0.5);
+	unsigned genomeSize(8);
+	unsigned int loops(2000); //Loops is the number of generations - can change safely
 
 	std::stringstream fileName;
-	fileName << "k=" << carryingCapacity << ",c=" << reach << ",sigma="<< standDeviation <<",loops="<< loops <<".dat";
+	fileName << "k=" << carryingCapacity << ",c=" << reach << ",sigma="<< standDeviation <<",genome size="<<genomeSize<<",loops="<< loops <<".dat";
 	std::cout.precision(3);
 	std::cout << "Evolving! (this could take some time)"<<std::endl;
-	org::environment test(100.0,1.0); //Create an environment with carrying capacity and range.
+	org::environment test(100.0,1.0,fileName.str()); //Create an environment with carrying capacity and range.
 	org::entity* temp;
 	org::genome* prototype;
 
 	for(unsigned int i(0);i<100;i++){
-		prototype=new org::genome(3);
+		prototype=new org::genome(genomeSize);
 		temp= new org::entity(0,*prototype);
 		test.addEntity(temp);
 	}
