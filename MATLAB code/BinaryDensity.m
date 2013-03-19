62853e542993c6da55a39f96f1f2e9d83caa9e16
TaranMoltu <taranmoltu@gmail.com>
2013-03-19 11:33:25 +0000

    Removed matlab code

--- MATLAB code/BinaryDensity.m
+++
@@ -1,7 +0,0 @@
-% BinaryDensity.m
-% After using BinarySplitter.m and drgui.m to get the variable mX which is
-% the reduced data, run this script to get the number of each type of
-% point.  Assumes the reduced data has been opened as mX.
-% M. Williams 11/12/2012
-
- 
